30639ff0c12204033a341fae58188b3968531b57
Axel Howind <axh@dua3.com>
2025-07-29 05:48:07 +0200

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "license"
-val projectVersion = "0.1.0-beta4"
+val projectVersion = "0.1.0-beta5-SNAPSHOT"
 
 include("license-app")
 
