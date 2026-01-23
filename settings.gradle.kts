f0fa56f7d154483de713b619fa147495358ec4c8
Axel Howind <axh@dua3.com>
2026-01-23 04:54:03 +0100

    bump version to 0.1.0-rc4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "license"
-val projectVersion = "0.1.0-rc3"
+val projectVersion = "0.1.0-rc4"
 
 dependencyResolutionManagement {
 
